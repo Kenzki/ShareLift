d804e11f0def611ac7de4a677f9d7a781d546e74
Kenneth <malonkenneth@yahoo.com>
2018-10-21 00:28:03 +0100

    Changes colour theme of the project

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,16 +1,16 @@
 /* GET 'home' page */
 const homelist = function(req, res){
   res.render('locations-list', {
-    title: 'Loc8r - find a place to work with wifi',
+    title: 'ShareLift - find lifts near you',
     pageHeader: {
-      title: 'Loc8r',
-      strapline: 'Find places to work with wifi near you!'
+      title: 'ShareLift',
+      strapline: 'Find lifts near you!'
     },
-    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+    sidebar: "ShareLift - Add a Journey or Find a Journey",
     locations: [{
-        name: 'Starcups',
+        name: 'Kenneth Malon',
         address: '125 High Street, Reading, RG6 1PS',
-        rating: 3,
+        rating: 4,
         facilities: ['Hot drinks', 'Food', 'Premium wifi'],
         distance: '100m'
     }, {
