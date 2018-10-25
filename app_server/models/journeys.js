c42e16d166844eb80621dcba3d286f9e38be76fe
Kenneth Malon <Kenneth.Malon@students.ittralee.ie>
2018-10-25 10:58:11 +0100

    made schema for database

---
+++ app_server/models/journeys.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const feedBackSchema = new mongoose.Schema({
+   author: String,
+   rating:{
+       type: Number,
+       'default': 0,
+       min: 0,
+       max: 5
+   },
+   feedbackText:String,
+   createOn: {type: Date, default:Date.now}
+});
+
+const journeySchema = new mongoose.Schema({
+   name: String,
+   departure: String,
+   destination: String,
+   address: String,
+    rating:{
+        type: Number,
+        'default': 0,
+        min: 0,
+        max: 5
+    },
+    date: String,
+    time: String,
+    coords: {
+       type: [Number],
+       index: '2dsphere'
+    },
+    descriptionText: String,
+    contact: String,
+    email: String,
+    feedback: [feedBackSchema]
+});
+
+mongoose.model('Journey', journeySchema);
+
+
+
+
+
+
